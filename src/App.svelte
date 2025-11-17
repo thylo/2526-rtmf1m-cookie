c44ac423d72d243ea816f0fcacfed571234bf3d9
Julien Moreau <julien@thylo.be>
2025-11-17 09:47:50 +0100

    message de commit

---
+++ src/App.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import Shop from "./components/shop/Shop.svelte";
+
+
+  let count = $state(0);
+
+  function onCookieClick(){
+    console.log('click');
+    count ++;
+  };
+</script>
+
+<main class="application">
+  <div class="frite-bar">
+    <p>Frites: {count}</p>
+    <button class="frite-button" onclick={onCookieClick}>
+      <img src="/frite.png" alt="frite">
+    </button>
+  </div>
+  <div class="inventory">
+  </div>
+  <div class="shop">
+    <Shop count={count} />
+  </div>
+</main>
+
+<style>
+  .application {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    height: 100%;
+    background: pink;
+    position: fixed;
+    width: 100%;
+  }
+  
+  .frite-bar{
+    background: black;
+    color: white;
+    font-size: 6.25em;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .frite-button{
+    background-color: transparent;
+    border-radius: 0%;
+    border: 0cap;
+  }
+
+  .inventory{
+    background: yellow;
+  }
+  .shop{
+    background: red;
+  }
+</style>
---
+++ src/components/shop/Shop.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+
+let {count} = $props();
+
+const products = [
+    {name: 'Pointeur', price: 10, id: 'cursor'},
+    {name: 'Moules', price: 20, id: 'moules'},
+    {name: 'Grand-mere', price: 30, id: 'grand_mere'},
+];
+
+</script>
+
+
+<h1>Shop :D</h1>
+
+<ul>
+    {#each products as product}
+        <li>
+            <button disabled={count < product.price}> {product.name} {product.price} frites
+                <img class="product-image" src={`/products/${product.id}.png`} alt="">
+            </button>
+        </li>
+    {/each}
+</ul>
+
+<style>
+    .product-image{
+        width: 100px;
+        aspect-ratio: 1;
+      object-fit: cover;
+    }
+</style>
